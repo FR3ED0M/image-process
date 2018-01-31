c09bd69f0ccd0153ab007c5b54effea935f231a1
L. Garcia <lgarcia2013@fau.edu>
2018-01-31 16:49:44 -0500

    Added classes, and made it to call the class to open pictures.

--- main.py
+++ main.py
@@ -12,71 +12,64 @@ def do_popup(event):
         popup.grab_release()
 
 
-def openPlains():
-    image = Image.open("plains.jpg")
-    image = image.resize((250, 250), Image.ANTIALIAS)
-    photo = ImageTk.PhotoImage(image)
-    label4 = Label(image=photo)
-    label4.image = photo
-    label4.place(x=5, y=75)
+class openImage:
+    def openPlains(self):
+        image = Image.open("plains.jpg")
+        image = image.resize((250, 250), Image.ANTIALIAS)
+        photo = ImageTk.PhotoImage(image)
+        label4 = Label(image=photo)
+        label4.image = photo
+        label4.place(x=5, y=75)
+
+    def openBoxy(self):
+        image = Image.open("boxy.jpg")
+        image = image.resize((250, 250), Image.ANTIALIAS)
+        photo = ImageTk.PhotoImage(image)
+        label4 = Label(image=photo)
+        label4.image = photo
+        label4.place(x=5, y=75)
+
+    def openColors(self):
+        image = Image.open("colors.jpg")
+        image = image.resize((250, 250), Image.ANTIALIAS)
+        photo = ImageTk.PhotoImage(image)
+        label4 = Label(image=photo)
+        label4.image = photo
+        label4.place(x=5, y=75)
+
+    def openSpace(self):
+        image = Image.open("space.jpg")
+        image = image.resize((250, 250), Image.ANTIALIAS)
+        photo = ImageTk.PhotoImage(image)
+        label4 = Label(image=photo)
+        label4.image = photo
+        label4.place(x=5, y=75)
+
+    def openPlasma(self):
+        image = Image.open("plasma.jpg")
+        image = image.resize((250, 250), Image.ANTIALIAS)
+        photo = ImageTk.PhotoImage(image)
+        label4 = Label(image=photo)
+        label4.image = photo
+        label4.place(x=5, y=75)
+        return
 
 
-def openBoxy():
-    image = Image.open("boxy.jpg")
-    image = image.resize((250, 250), Image.ANTIALIAS)
-    photo = ImageTk.PhotoImage(image)
-    label4 = Label(image=photo)
-    label4.image = photo
-    label4.place(x=5, y=75)
-
-
-def openColors():
-    image = Image.open("colors.jpg")
-    image = image.resize((250, 250), Image.ANTIALIAS)
-    photo = ImageTk.PhotoImage(image)
-    label4 = Label(image=photo)
-    label4.image = photo
-    label4.place(x=5, y=75)
-
-
-def openSpace():
-    image = Image.open("space.jpg")
-    image = image.resize((250, 250), Image.ANTIALIAS)
-    photo = ImageTk.PhotoImage(image)
-    label4 = Label(image=photo)
-    label4.image = photo
-    label4.place(x=5, y=75)
-
-
-def openPlasma():
-    image = Image.open("plasma.jpg")
-    image = image.resize((250, 250), Image.ANTIALIAS)
-    photo = ImageTk.PhotoImage(image)
-    label4 = Label(image=photo)
-    label4.image = photo
-    label4.place(x=5, y=75)
-    return image
-
-
-def renderfy():
-    photo = openPlasma()
-    label8 = Label(image=photo)
-    label8.image = photo
-    label8.place(x=350, y=75)
-
-
-
-# def showNew():
-
+# def renderfy():
+#     photo = openImage.openPlasma()
+#     label8 = Label(image=photo)
+#     label8.image = photo
+#     label8.place(x=350, y=75)
 
 
 # create a popup menu
+x = openImage()
 popup = Menu(parent, tearoff=0)
-popup.add_command(label="Boxy", command=openBoxy)
-popup.add_command(label="Colors", command=openColors)
-popup.add_command(label="Space", command=openSpace)
-popup.add_command(label="Plains", command=openPlains)
-popup.add_command(label="Plasma", command=openPlasma)
+popup.add_command(label="Boxy", command=x.openPlasma)
+popup.add_command(label="Colors", command=x.openColors)
+popup.add_command(label="Space", command=x.openSpace)
+popup.add_command(label="Plains", command=x.openPlains)
+popup.add_command(label="Plasma", command=x.openPlasma)
 
 
 # def openImage():
@@ -102,7 +95,7 @@ frame = ttk.Frame(parent, borderwidth=5)
 # buttons
 select = ttk.Button(parent, text="Select Image")
 select.bind("<Button-1>", do_popup)
-mess_with = ttk.Button(parent, text="Process Image", command=renderfy)
+mess_with = ttk.Button(parent, text="Process Image")
 
 
 # placement geometry
